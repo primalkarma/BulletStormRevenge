2b5012c94789eb9d319782ce8b33e4fa9c2f1050
primalkarma <primalkarma@gmail.com>
2025-07-29 11:40:33 +0530

    Initial Commit

---
+++ BulletStormRevenge/BulletStormRevengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulletStormRevengeApp.swift
+//  BulletStormRevenge
+//
+//  Created by Parimal Devi on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BulletStormRevengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
